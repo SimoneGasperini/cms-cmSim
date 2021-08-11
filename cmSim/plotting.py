6ddcae1a650a19392cbcdb3537c51a959f9f3eec
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-08-11 15:55:23 +0200

    Minor fix in plot_storage_history

--- cmSim/plotting.py
+++ cmSim/plotting.py
@@ -6,7 +6,7 @@ from datetime import date
 
 def plot_storage_history(sites, start_date=date(2019, 1, 1), end_date=date(2020, 12, 31), freq='M'):
     timeline = pd.date_range(start=start_date, end=end_date, freq=freq)
-    data = {s.name: s.get_storage_history_nofor(timeline) for s in sites}
+    data = {s.name: s.get_storage_history(timeline) for s in sites}
     fig, ax = plt.subplots(figsize=(12, 9))
     ax.stackplot(timeline, data.values(), labels=data.keys())
     ax.set_ylabel('stored data [B]', fontsize=14)
