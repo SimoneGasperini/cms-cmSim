3af939fe797e73b778ea36fef2cb6f5af27abe6b
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-08-12 17:03:39 +0200

    Remove unnecessary imports and requirements

--- cmSim/site.py
+++ cmSim/site.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-
-
 def get_sites(df, tier=None, country=None):
     tier = tier + '_' if tier is not None else ''
     country = '_' + country + '_' if country is not None else ''
