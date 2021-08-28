12816ae471dc4de0547ef00dd7ca189310203bcf
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-08-28 20:05:19 +0200

    Add setup.py to create cmSim package
    This is needed to use cmSim classes and call cmSim functions
    from the jupyter notebooks in their directory.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='CMS-cmSim',
+    url='https://github.com/SimoneGasperini/cms-cmSim',
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
