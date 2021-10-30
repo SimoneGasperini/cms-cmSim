17192fdeb39e029aca2cd6d8b1da85e8a2182af6
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-10-30 11:54:51 +0200

    Reimplement Site class inheriting from Base
    Classmethod from_dataframe and constructor are updated.
    Utils functions are called to get Site's properties.

--- cmSim/site.py
+++ cmSim/site.py
@@ -1,122 +1,76 @@
-def get_sites(df, tier=None, country=None):
+from cmSim._base import Base
+from cmSim import utils
+
+
+class Site(Base):
     """
-    Return a list of Site objects created from the given dataframe and applying the
-    optional filters by tier and by country.
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        Dataframe from which to extract data about the sites
-    tier : str, optional
-        Tier of belonging (e.g. 'T2'), by default None
-    country : str, optional
-        Country of belonging (e.g. 'IT'), by default None
-
-    Returns
-    -------
-    List[Site]
-        List containing the selected Site objects
-    """
-    tier = tier + '_' if tier is not None else ''
-    country = '_' + country + '_' if country is not None else ''
-    df = df[(df['node_name'].str.contains(tier)) &
-            (df['node_name'].str.contains(country))]
-    sites = [Site.from_dataframe(df=df[df['node_name'] == name])
-             for name in df['node_name'].unique()]
-    return sites
-
-
-class Site:
-    """
-    Class representing a computing centre/node/site in the world.
+    Class representing a node/site in the world.
 
     Parameters
     ----------
     name : str
-        Name
-    disk : float, optional
-        Total disk storage available, by default None
-    tape : float, optional
-        Total tape storage available, by default None
-    cpu : float, optional
-        Total CPU power available, by default None
-    df : pandas.DataFrame, optional
-        Dataframe containing data about the site, by default None
+        Site's name
+    data : pandas.DataFrame, optional
+        Data about the given site, by default None
     """
 
-    def __init__(self, name, disk=None, tape=None, cpu=None, df=None):
+    def __init__(self, name, data=None):
         self.name = name
-        self.disk = disk
-        self.tape = tape
-        self.cpu = cpu
-        self.df = df
+        self.data = data
 
     def __repr__(self):
         class_name = self.__class__.__qualname__
         params = list(self.__init__.__code__.co_varnames)
         params.remove('self')
-        params.remove('df')
+        params.remove('data')
         args = [f'{key}={getattr(self, key)}' for key in params]
         args.append(f'tier={self.tier}')
         args.append(f'country={self.country}')
         return f'{class_name}({", ".join(args)})'
 
     @classmethod
-    def from_dataframe(cls, df):
+    def from_dataframe(cls, name, df):
         """
-        Return the Site object created from data in the given dataframe.
+        Return the Site object created filtering data in the given dataframe.
 
         Parameters
         ----------
+        name : str
+            Site's name
         df : pandas.DataFrame
-            Dataframe containing data about the site
+            Input dataframe
 
         Returns
         -------
-        Site
-            Site object created
+        site : Site
+            Site object
         """
-        name = df['node_name'].iloc[0]
-        return cls(name=name, df=df)
+        df_site = df[df['node_name'] == name]
+        site = cls(name=name, data=df_site)
+        return site
 
     @property
     def tier(self):
         """
-        Get the Site's tier of belonging.
+        Get the tier of belonging.
 
         Returns
         -------
-        str
-            Tier of belonging
+        tier : str
+            Site's tier
         """
-        return self.name.split('_')[0]
+        tier = utils.get_tier_from_site(self.name)
+        return tier
 
     @property
     def country(self):
         """
-        Get the Site's country of belonging.
+        Get the country of belonging.
 
         Returns
         -------
-        str
-            Country of belonging
+        country : str
+            Site's country
         """
-        return self.name.split('_')[1]
-
-    def get_storage_history(self, timeline):
-        """
-        Return a list of stored data amounts corresponding to the given timeline.
-
-        Parameters
-        ----------
-        timeline : pandas.DatetimeIndex
-            Timeline object by Pandas
-
-        Returns
-        -------
-        List[float]
-            List of stored data amounts
-        """
-        size = [self.df[(date >= self.df['min_time']) & (
-            date <= self.df['max_time'])]['rep_size'].sum() for date in timeline]
-        return size
+        country = utils.get_country_from_site(self.name)
+        return country
