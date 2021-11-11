584066464241fbe192732a039cc881e93d41e426
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-11-11 14:27:31 +0100

    Minor fix for package installation

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    with open('./requirements.txt', mode='r') as f:
+        reqs = f.read().splitlines()
+    return reqs
 
 
 setup(
     name='CMS-cmSim',
+    author='SimoneGasperini',
+    author_email='simone.gasperini2@studio.unibo.it',
+    version='0.0.1',
     url='https://github.com/SimoneGasperini/cms-cmSim',
     packages=find_packages(),
-    python_requires=">=3.6",
+    install_requires=get_requirements(),
+    python_requires='>=3.8',
 )
