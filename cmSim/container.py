35e9f98e0381c88dff556028bb8d8c2ae6f7d3d2
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-11-18 18:35:30 +0100

    Update plotting methods in container module

--- cmSim/container.py
+++ cmSim/container.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
 from datetime import date
 from cmSim._base import Base
 from cmSim import utils
+from cmSim.tools import plotting
 
 
 def get_datasets(df, pwgs=None, datatiers=None):
@@ -70,7 +72,7 @@ class DataContainer(Base):
         container = cls(datasets=datasets, data=df)
         return container
 
-    def plot_storage_history_in_sites(self, ax, tier=None, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
+    def plot_storage_history_in_sites(self, ax, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
         """
         Draw a stacked area plot representing the time series of data stored on disk (grouped by
         site) over the given time period (from 'date1' to 'date2' with intervals given by 'freq').
@@ -82,6 +84,8 @@ class DataContainer(Base):
             Matplotlib axes on which to draw the plot
         tier : str, optional
             Tier to be considered (if None, all are taken), by default None
+        norm : bool, optional
+            If True, apply normalization, by default False
         date1 : datetime.date, optional
             Timeline starting date, by default date(2019, 1, 1)
         date2: datetime.date, optional
@@ -90,20 +94,28 @@ class DataContainer(Base):
             Timeline frequency (month: 'M', week: 'W', etc), by default 'W'
         """
         df = self._filter_by_tier(self.data, tier)
-        sites = list(df['node_name'].unique())
+        sites = sorted(list(df['node_name'].unique()))
         timeline = [dt.date() for dt in pd.date_range(date1, date2, freq=freq)]
-        data = []
-        for site in sites:
-            dframe = df[df['node_name'] == site]
-            storage_history = self._get_storage_history(dframe, timeline)
-            data.append(storage_history)
-        ax.stackplot(timeline, data, labels=sites)
-        tiers = 'T1-T2' if tier is None else tier
-        ax.set_title(f'Data storage by sites (tiers={tiers})', fontsize=20)
-        ax.legend(loc='best', fontsize=10, ncol=3)
-        self._plot_settings(ax=ax, legend=False)
+        time_series = [self._get_storage_history(df[df['node_name'] == site], timeline)
+                       for site in sites]
+        if norm:
+            time_series = plotting.norm_stacked_areas(time_series)
+            ylabel = 'Data fraction'
+        else:
+            ylabel = 'Data amount [B]'
+        time_series, sorted_labels = plotting.sort_stacked_areas(
+            time_series=time_series, labels=sites)
+        time_series, merged_sorted_labels = plotting.merge_stacked_areas(
+            time_series=time_series, labels=sorted_labels)
+        colors = plotting.get_default_colors(labels=merged_sorted_labels)
+        ax.stackplot(timeline, time_series,
+                     labels=merged_sorted_labels, colors=colors)
+        labels = [site for site in sites
+                  if site in merged_sorted_labels] + ['Other']
+        plotting.set_stackplot_settings(
+            ax, ylabel=ylabel, legend_title='Sites', legend_labels=labels)
 
-    def plot_storage_history_in_countries(self, ax, tier=None, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
+    def plot_storage_history_in_countries(self, ax, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
         """
         Draw a stacked area plot representing the time series of data stored on disk (grouped by
         country) over the given time period (from 'date1' to 'date2' with intervals given by 'freq').
@@ -115,6 +127,8 @@ class DataContainer(Base):
             Matplotlib axes on which to draw the plot
         tier : str, optional
             Tier to be considered (if None, all are taken), by default None
+        norm : bool, optional
+            If True, apply normalization, by default False
         date1 : datetime.date, optional
             Timeline starting date, by default date(2019, 1, 1)
         date2: datetime.date, optional
@@ -123,22 +137,30 @@ class DataContainer(Base):
             Timeline frequency (month: 'M', week: 'W', etc), by default 'W'
         """
         df = self._filter_by_tier(self.data, tier)
-        countries = list(set(utils.get_country_from_site(site)
-                             for site in df['node_name'].unique()))
+        countries = utils.get_countries()
+        country_mask = np.array([utils.get_country_from_site(site)
+                                 for site in df['node_name']])
         timeline = [dt.date() for dt in pd.date_range(date1, date2, freq=freq)]
-        data = []
-        for country in countries:
-            code = utils.get_countryCode_from_countryName(country)
-            dframe = df[df['node_name'].str.contains(f'_{code}_')]
-            storage_history = self._get_storage_history(dframe, timeline)
-            data.append(storage_history)
-        ax.stackplot(timeline, data, labels=countries)
-        tiers = 'T1-T2' if tier is None else tier
-        ax.set_title(f'Data storage by countries (tiers={tiers})', fontsize=20)
-        ax.legend(loc='best', fontsize=12, ncol=2)
-        self._plot_settings(ax=ax, legend=False)
+        time_series = [self._get_storage_history(df[country_mask == country], timeline)
+                       for country in countries]
+        if norm:
+            time_series = plotting.norm_stacked_areas(time_series)
+            ylabel = 'Data fraction'
+        else:
+            ylabel = 'Data amount [B]'
+        time_series, sorted_labels = plotting.sort_stacked_areas(
+            time_series=time_series, labels=countries)
+        time_series, merged_sorted_labels = plotting.merge_stacked_areas(
+            time_series=time_series, labels=sorted_labels)
+        colors = plotting.get_default_colors(labels=merged_sorted_labels)
+        ax.stackplot(timeline, time_series,
+                     labels=merged_sorted_labels, colors=colors)
+        labels = [country for country in countries
+                  if country in merged_sorted_labels] + ['Other']
+        plotting.set_stackplot_settings(
+            ax, ylabel=ylabel, legend_title='Countries', legend_labels=labels)
 
-    def plot_storage_history_in_datalakes(self, ax, tier=None, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
+    def plot_storage_history_in_datalakes(self, ax, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
         """
         Draw a stacked area plot representing the time series of data stored on disk (grouped by
         datalake) over the given time period (from 'date1' to 'date2' with intervals given by 'freq').
@@ -150,6 +172,8 @@ class DataContainer(Base):
             Matplotlib axes on which to draw the plot
         tier : str, optional
             Tier to be considered (if None, all are taken), by default None
+        norm : bool, optional
+            If True, apply normalization, by default False
         date1 : datetime.date, optional
             Timeline starting date, by default date(2019, 1, 1)
         date2: datetime.date, optional
@@ -159,14 +183,21 @@ class DataContainer(Base):
         """
         df = self._filter_by_tier(self.data, tier)
         datalakes = utils.get_datalakes()
-        df['datalake'] = df['node_name'].apply(utils.get_datalake_from_site)
         timeline = [dt.date() for dt in pd.date_range(date1, date2, freq=freq)]
-        data = []
-        for datalake in datalakes:
-            dframe = df[df['datalake'] == datalake]
-            storage_history = self._get_storage_history(dframe, timeline)
-            data.append(storage_history)
-        ax.stackplot(timeline, data, labels=datalakes)
-        tiers = 'T1-T2' if tier is None else tier
-        ax.set_title(f'Data storage by datalakes (tiers={tiers})', fontsize=20)
-        self._plot_settings(ax=ax)
+        datalake_mask = np.array([utils.get_datalake_from_site(site)
+                                  for site in df['node_name']])
+        time_series = [self._get_storage_history(df[datalake_mask == datalake], timeline)
+                       for datalake in datalakes]
+        if norm:
+            time_series = plotting.norm_stacked_areas(time_series)
+            ylabel = 'Data fraction'
+        else:
+            ylabel = 'Data amount [B]'
+        time_series, sorted_labels = plotting.sort_stacked_areas(
+            time_series=time_series, labels=datalakes)
+        sorted_colors = plotting.get_custom_colors(
+            labels=sorted_labels, groups='datalakes')
+        ax.stackplot(timeline, time_series,
+                     labels=sorted_labels, colors=sorted_colors)
+        plotting.set_stackplot_settings(
+            ax, ylabel=ylabel, legend_title='Datalakes', legend_labels=datalakes)
