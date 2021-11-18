dd9c68933e8782ec89be5a5ebf134816098746d2
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-11-18 11:12:08 +0100

    Add new piechart plotting function and corresponding notebook

--- cmSim/tools/plotting.py
+++ cmSim/tools/plotting.py
@@ -43,3 +43,23 @@ def _sort_legend_labels(ax, labels):
     sorted_handles = np.array(_handles)[sorting]
     sorted_labels = np.array(_labels)[sorting]
     return sorted_handles, sorted_labels
+
+
+def plot_piechart_by_pag(ax, df, pags, datatiers=None):
+    if datatiers is not None:
+        df = df[df['tier'].isin(datatiers)]
+    data = [df[df['pwg'] == pag]['dsize'].sum() for pag in pags]
+    # Other PWG
+    df_other = df[(df['pwg'] != 'None') & (~df['pwg'].isin(pags))]
+    data.append(df_other['dsize'].sum())
+    # PWG not found
+    df_not_found = df[df['pwg'] == 'None']
+    data.append(df_not_found['dsize'].sum())
+    labels = pags + ['Other PWG', 'Not found']
+    sorted_colors = get_colors(labels=labels, groups='pags')
+    ax.pie(data, labels=labels, colors=sorted_colors, normalize=True, explode=[0.1]*len(data),
+           autopct='%1.1f%%', pctdistance=1.1, textprops={'fontsize': 16}, labeldistance=None)
+    total = round(df['dsize'].sum() / 1e15, 3)
+    ax.set_title(f'Total size = {total} PB', fontsize=20)
+    ax.legend(title='PAGs', title_fontsize=18, loc='center left',
+              bbox_to_anchor=(1, 0, 0.5, 1), fontsize=16)
