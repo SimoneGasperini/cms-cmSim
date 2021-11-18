717ce21c64bd947f1fa786973f21196545df665f
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-11-18 18:23:58 +0100

    Add functions to manage stacked areas in plotting module

--- cmSim/tools/plotting.py
+++ cmSim/tools/plotting.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pylab as plt
 from cmSim import utils
 
 
@@ -12,14 +13,37 @@ def norm_stacked_areas(time_series):
 def sort_stacked_areas(time_series, labels, more_labels=[]):
     time_series = np.array(time_series)
     labels = np.array(labels)
-    l = len(labels)
-    sorting = time_series[:l].mean(axis=1).argsort()[::-1]
-    sorted_time_series = np.vstack((time_series[:l][sorting], time_series[l:]))
+    n = len(labels)
+    sorting = time_series[:n].mean(axis=1).argsort()[::-1]
+    sorted_time_series = np.vstack((time_series[:n][sorting], time_series[n:]))
     sorted_labels = np.append(labels[sorting], more_labels)
     return sorted_time_series, sorted_labels
 
 
-def get_colors(labels, groups):
+def merge_stacked_areas(time_series, labels):
+    if len(time_series) > 9:
+        time_series = np.vstack((time_series[:9],
+                                 time_series[9:].sum(axis=0)))
+        labels = np.append(labels[:9],
+                           'Other')
+    return time_series, labels
+
+
+def get_default_colors(labels):
+    n = len(labels)
+    if n > 10:
+        raise ValueError('Maximum number of distinct labels is 9 (+ "Other")')
+    else:
+        colors = [plt.cm.tab10(i) for i in range(10)]
+        if 'Other' in labels:
+            i = np.where(labels == 'Other')[0][0]
+            colors[i], colors[7] = colors[7], colors[9]
+        else:
+            colors[7] = colors[9]
+    return colors[:n]
+
+
+def get_custom_colors(labels, groups):
     if groups == 'pags':
         lab_to_col = utils.get_pag_to_color()
     if groups == 'datatiers':
@@ -34,7 +58,7 @@ def set_stackplot_settings(ax, ylabel, legend_title, legend_labels):
     ax.tick_params(axis='both', labelsize=14)
     ax.set_ylabel(ylabel, fontsize=18)
     handles, labels = _sort_legend_labels(ax, legend_labels)
-    ax.legend(handles, labels, title=legend_title, title_fontsize=18,
+    ax.legend(handles, labels, title=legend_title, title_fontsize=20,
               loc='center left', bbox_to_anchor=(1, 0.5), fontsize=16)
     ax.grid(linestyle='dotted')
 
@@ -58,7 +82,7 @@ def plot_piechart_by_pag(ax, df, pags, datatiers=None):
     df_not_found = df[df['pwg'] == 'None']
     data.append(df_not_found['dsize'].sum())
     labels = pags + ['Other PWG', 'Not found']
-    sorted_colors = get_colors(labels=labels, groups='pags')
+    sorted_colors = get_custom_colors(labels=labels, groups='pags')
     ax.pie(data, labels=labels, colors=sorted_colors, normalize=True, explode=[0.1]*len(data),
            autopct='%1.1f%%', pctdistance=1.1, textprops={'fontsize': 16}, labeldistance=None)
     total = round(df['dsize'].sum() / 1e15, 3)
