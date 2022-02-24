495548fa7834deef88b2254a3f03c27fb2c8f29a
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2022-02-24 18:21:18 +0100

    Add functions to plot disk storage grouped by campaign year and generator

--- cmSim/_base.py
+++ cmSim/_base.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+import pylab as plt
 from datetime import date
+from cmSim import utils
 from cmSim.tools import plotting
 
 
@@ -9,10 +11,6 @@ class Base:
     @staticmethod
     def _filter_by_tier(df, tier):
         return df[df['node_name'].str.startswith(tier)] if tier is not None else df
-
-    @staticmethod
-    def _filter_by_datatiers(df, datatiers):
-        return df[df['tier'].isin(datatiers)] if datatiers is not None else df
 
     @staticmethod
     def _get_storage_history(df, timeline):
@@ -67,7 +65,7 @@ class Base:
         plotting.set_stackplot_settings(
             ax, ylabel=ylabel, legend_title='Data tiers', legend_labels=labels)
 
-    def plot_storage_history_by_pag(self, ax, pags, datatiers=None, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
+    def plot_storage_history_by_pag(self, ax, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
         """
         Draw a stacked area plot representing the time series of data stored on disk (grouped by PAG)
         over the given time period (from 'date1' to 'date2' with intervals given by 'freq').
@@ -77,10 +75,6 @@ class Base:
         ----------
         ax : matplotlib.axes
             Matplotlib axes on which to draw the plot
-        pags : List[str]
-            PAGs to be considered in the grouping
-        datatiers : List[str], optional
-            Datatiers to be considered (if None, all are taken), by default None
         tier : str, optional
             Tier to be considered (if None, all are taken), by default None
         norm : bool, optional
@@ -92,9 +86,9 @@ class Base:
         freq : str, optional
             Timeline frequency (month: 'M', week: 'W', etc), by default 'W'
         """
-        df = self._filter_by_datatiers(self.data, datatiers)
-        df = self._filter_by_tier(df, tier)
+        df = self._filter_by_tier(self.data, tier)
         timeline = [dt.date() for dt in pd.date_range(date1, date2, freq=freq)]
+        pags = list(set(df['pwg']) & set(utils.get_pags()))
         time_series = [self._get_storage_history(df[df['pwg'] == pag], timeline)
                        for pag in pags]
         # Other PWG
@@ -105,18 +99,112 @@ class Base:
         storage_history = self._get_storage_history(
             df[df['pwg'] == 'None'], timeline)
         time_series.append(storage_history)
-        more_labels = ['Other PWG', 'Not found']
-        labels = pags + more_labels
+        time_series = np.array(time_series) / 1e15
         if norm:
             time_series = plotting.norm_stacked_areas(time_series)
             ylabel = 'Data fraction'
         else:
-            ylabel = 'Data amount [B]'
-        time_series, sorted_labels = plotting.sort_stacked_areas(
-            time_series=time_series, labels=pags, more_labels=more_labels)
-        sorted_colors = plotting.get_custom_colors(
-            labels=sorted_labels, groups='pags')
-        ax.stackplot(timeline, time_series,
-                     labels=sorted_labels, colors=sorted_colors)
-        plotting.set_stackplot_settings(
-            ax, ylabel=ylabel, legend_title='PAGs', legend_labels=labels)
+            ylabel = 'Data amount [PB]'
+        time_series, labels = plotting.sort_stacked_areas(
+            time_series=time_series, labels=pags, more_labels=['Other PWG', 'Not found'])
+        colors = plotting.get_custom_colors(labels=labels, groups='pags')
+        ax.stackplot(timeline, time_series, labels=labels, colors=colors)
+        plotting.set_legend_settings(ax, title='PAGs', labels=labels)
+        ax.tick_params(axis='both', which='major', labelsize=18)
+        ax.set_ylabel(ylabel, fontsize=24)
+        ax.grid(linestyle='dotted')
+
+    def plot_storage_history_by_year(self, ax, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
+        """
+        Draw a stacked area plot representing the time series of data stored on disk (grouped by campaign year)
+        over the given time period (from 'date1' to 'date2' with intervals given by 'freq').
+        The data are filtered considering only replicas stored in sites belonging to the specified 'tier'.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes
+            Matplotlib axes on which to draw the plot
+        tier : str, optional
+            Tier to be considered (if None, all are taken), by default None
+        norm : bool, optional
+            If True, apply normalization, by default False
+        date1 : datetime.date, optional
+            Timeline starting date, by default date(2019, 1, 1)
+        date2: datetime.date, optional
+            Timeline ending date, by default date(2020, 12, 31)
+        freq : str, optional
+            Timeline frequency (month: 'M', week: 'W', etc), by default 'W'
+        """
+        df = self._filter_by_tier(self.data, tier)
+        timeline = [dt.date() for dt in pd.date_range(date1, date2, freq=freq)]
+        years = list(df['year'].unique())
+        if 'None' in years:
+            years.remove('None')
+        time_series = [self._get_storage_history(df[df['year'] == year], timeline)
+                       for year in years]
+        # Year not found
+        storage_history = self._get_storage_history(
+            df[df['year'] == 'None'], timeline)
+        time_series.append(storage_history)
+        time_series = np.array(time_series) / 1e15
+        if norm:
+            time_series = plotting.norm_stacked_areas(time_series)
+            ylabel = 'Data fraction'
+        else:
+            ylabel = 'Data amount [PB]'
+        time_series, labels = plotting.sort_stacked_areas(
+            time_series=time_series, labels=years, more_labels=['Not found'])
+        colors = plotting.get_default_colors(labels=labels, cmap=plt.cm.Dark2)
+        ax.stackplot(timeline, time_series, labels=labels, colors=colors)
+        labels.sort()
+        plotting.set_legend_settings(ax, title='Campaign year', labels=labels)
+        ax.tick_params(axis='both', which='major', labelsize=18)
+        ax.set_ylabel(ylabel, fontsize=24)
+        ax.grid(linestyle='dotted')
+
+    def plot_storage_history_by_generator(self, ax, tier=None, norm=False, date1=date(2019, 1, 1), date2=date(2020, 12, 31), freq='W'):
+        """
+        Draw a stacked area plot representing the time series of data stored on disk (grouped by MC generator)
+        over the given time period (from 'date1' to 'date2' with intervals given by 'freq').
+        The data are filtered considering only replicas stored in sites belonging to the specified 'tier'.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes
+            Matplotlib axes on which to draw the plot
+        tier : str, optional
+            Tier to be considered (if None, all are taken), by default None
+        norm : bool, optional
+            If True, apply normalization, by default False
+        date1 : datetime.date, optional
+            Timeline starting date, by default date(2019, 1, 1)
+        date2: datetime.date, optional
+            Timeline ending date, by default date(2020, 12, 31)
+        freq : str, optional
+            Timeline frequency (month: 'M', week: 'W', etc), by default 'W'
+        """
+        df = self._filter_by_tier(self.data, tier)
+        timeline = [dt.date() for dt in pd.date_range(date1, date2, freq=freq)]
+        generators = list(df['generator'].unique())
+        if 'None' in generators:
+            generators.remove('None')
+        time_series = [self._get_storage_history(df[df['generator'] == generator], timeline)
+                       for generator in generators]
+        # MC generator not found
+        storage_history = self._get_storage_history(
+            df[df['generator'] == 'None'], timeline)
+        time_series.append(storage_history)
+        time_series = np.array(time_series) / 1e15
+        if norm:
+            time_series = plotting.norm_stacked_areas(time_series)
+            ylabel = 'Data fraction'
+        else:
+            ylabel = 'Data amount [PB]'
+        time_series, labels = plotting.sort_stacked_areas(
+            time_series=time_series, labels=generators, more_labels=['Not found'])
+        colors = plotting.get_default_colors(labels=labels, cmap=plt.cm.Set3)
+        ax.stackplot(timeline, time_series, labels=labels, colors=colors)
+        plotting.set_legend_settings(ax, title='MC generators', labels=labels)
+        ax.tick_params(axis='both', which='major', labelsize=18)
+        ax.set_ylabel(ylabel, fontsize=24)
+        ax.grid(linestyle='dotted')
