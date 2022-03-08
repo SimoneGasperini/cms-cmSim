d11fe144e26a26fc9a02089339e4047f7e6977e3
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2022-03-08 16:03:03 +0100

    Update popularity data preprocessing script to have daily info

--- scripts/preprocess_pop_data.py
+++ scripts/preprocess_pop_data.py
@@ -1,219 +1,195 @@
 import os
 import json
-import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
 from cmSim import utils
 
-tqdm.pandas()
-sim_tiers = ['AODSIM', 'MINIAODSIM', 'NANOAODSIM']
+import warnings
+warnings.filterwarnings('ignore')
 
 
-def create_datasets_list_sim_parquet():
-    print(f'Creating "datasets_list_sim.parquet"...', flush=True, end='')
-    df = pd.read_parquet(
-        './../data/parquet/dataset_size_info.parquet').dropna()
-    df = df[df['dsize'] > 0.]
-    df = df[df['nfiles'] > 0]
-    df = df[df['devts'] > 0]
-    df = df[df['tier'].isin(sim_tiers)]
-    df.reset_index(drop=True).to_parquet('./../data/datasets_list_sim.parquet')
+def create_pop_directories(datatier_to_outdir):
+    print('\nCreating popularity directories...', end='')
+    for dt in datatier_to_outdir:
+        dir = datatier_to_outdir[dt]
+        if not os.path.exists(dir):
+            os.makedirs(dir)
     print(' Done')
 
 
-def create_data_replicas_sim_parquet():
-    print(f'Creating "data_replicas_sim.parquet"...', flush=True, end='')
-    df = pd.read_parquet('./../data/parquet/dataset_site_info.parquet')
-    df = df.drop(
-        columns=['dataset_id', 'replica_time_create', 'br_user_group_id']).dropna()
-    df = df[df['tier'].isin(sim_tiers)]
-    for col in ['min_time', 'max_time']:
-        df[col] = df[col].apply(lambda date: date.strftime('%Y%m%d'))
-    df.reset_index(drop=True).to_parquet('./../data/data_replicas_sim.parquet')
-    print(' Done')
+# Data Bookkeeping Service
+def create_dbs_features_json(dbs_file, datatier_to_outdir):
+    df = pd.read_parquet(dbs_file)
+    df.dropna(inplace=True)
+    df = df[(df['dsize'] > 0.) & (df['nfiles'] > 0) & (df['devts'] > 0)]
+    for datatier in datatier_to_outdir:
+        message = f'\nCreating "{datatier}/dbs_features.json" file...'
+        print(message, flush=True)
+        dframe = df[df['tier'] == datatier.upper()]
+        data = {row['d_dataset']: {'tot_size': round(row['dsize']),
+                                   'avg_file_size': round(row['dsize'] / row['nfiles']),
+                                   'avg_event_size': round(row['dsize'] / row['devts'])}
+                for _, row in tqdm(dframe.iterrows())}
+        outfile = datatier_to_outdir[datatier] + 'dbs_features.json'
+        with open(outfile, mode='w') as f:
+            json.dump(data, f, indent=2)
 
 
-def create_data_accesses_sim_parquet():
-    print(f'Creating "data_accesses_sim.parquet"...', flush=True, end='')
-    df = pd.read_parquet('./../data/parquet/dataset_reads.parquet').dropna()
-    df['tier'] = df['d_dataset'].apply(lambda name: name.split('/')[-1])
-    df = df[df['tier'].isin(sim_tiers)]
-    df['num_accesses'] = df['fract_read'].apply(utils.get_rounded_num_accesses)
-    df.reset_index(drop=True).to_parquet('./../data/data_accesses_sim.parquet')
-    print(' Done')
-
-
-def create_datasets_mcm_info_json():
-    for sim_tier in sim_tiers:
-        filepath = f'./../data/mcm/{sim_tier.lower()}_mcm_data.json'
-        mcm_data = utils.get_mcm_data(filepath)
-        temp_data = {dataset: {'pwg': utils.get_pwg_from_dataset(dataset, mcm_data),
-                               'campaign': utils.get_campaign_from_dataset(dataset, mcm_data),
-                               'generator': utils.get_generator_from_dataset(dataset, mcm_data)}
-                     for dataset in tqdm(mcm_data, desc=f'Getting {sim_tier} datasets info')}
-        with open(f'./../data/{sim_tier.lower()}_temp.json', mode='w') as f:
-            json.dump(temp_data, f)
-        mcm_data = None
-        temp_data = None
-    print(f'Creating "datasets_mcm_info.json"...', flush=True, end='')
-    data = {}
-    for sim_tier in sim_tiers:
-        filename = f'./../data/{sim_tier.lower()}_temp.json'
-        with open(filename, mode='r') as f:
-            data.update(json.load(f))
-        os.remove(filename)
-    with open(f'./../data/datasets_mcm_info.json', mode='w') as file:
-        json.dump(data, file, indent=2)
-    print(' Done')
-
-
-def create_data_replicas_on_disk_json():
-    print(f'Creating "data_replicas_on_disk.json"...', flush=True)
-    df = pd.read_parquet('./../data/data_replicas_sim.parquet')
-    data = {dataset: {} for dataset in set(df['dataset_name'])}
-    for dataset, dframe in tqdm(df.groupby('dataset_name')):
-        data[dataset]['intervals'] = set(
-            zip(dframe['min_time'], dframe['max_time']))
-        data[dataset]['months'] = set()
-        for start, end in data[dataset]['intervals']:
-            dt = datetime.strptime(start, '%Y%m%d').date()
-            data[dataset]['months'].add(dt.strftime('%Y-%m'))
-            while dt <= datetime.strptime(end, '%Y%m%d').date():
-                dt += relativedelta(months=1)
-                data[dataset]['months'].add(dt.strftime('%Y-%m'))
-        data[dataset]['intervals'] = list(data[dataset]['intervals'])
-        data[dataset]['months'] = sorted(list(data[dataset]['months']))
-    with open('./../data/data_replicas_on_disk.json', mode='w') as file:
-        json.dump(data, file, indent=2)
-
-
-def filter(row, data):
-    for t1, t2 in data[row['d_dataset']]['intervals']:
-        date1 = datetime.strptime(t1, '%Y%m%d').date()
-        date = datetime.strptime(row['day'], '%Y%m%d').date()
-        date2 = datetime.strptime(t2, '%Y%m%d').date()
-        if date1 - timedelta(days=1) <= date <= date2:
-            return True
-    return False
-
-
-def remove_bad_accesses_from_parquet():
-    print(f'Removing bad accesses from "data_accesses_sim.parquet"...', flush=True)
-    df = pd.read_parquet('./../data/data_accesses_sim.parquet')
-    with open('./../data/data_replicas_on_disk.json') as file:
-        data = json.load(file)
-    df = df[df['d_dataset'].isin(set(data.keys()))]
-    df = df[df.progress_apply(lambda row: filter(row, data), axis=1)]
-    df.reset_index(drop=True).to_parquet('./../data/data_accesses_sim.parquet')
-
-
-def create_data_accesses_by_month_json():
-    print(f'Creating "data_accesses_by_month.json"...', flush=True)
-    with open('./../data/data_replicas_on_disk.json') as file:
-        replicas = json.load(file)
-    df = pd.read_parquet('./../data/data_accesses_sim.parquet')
-    df['month'] = df['day'].apply(lambda day: f'{day[:4]}-{day[4:6]}')
-    data = {dataset: {month: 0 for month in replicas[dataset]['months']}
-            for dataset in df['d_dataset'].unique()}
-    for _, row in tqdm(df.iterrows()):
-        d, m, n = row['d_dataset'], row['month'], row['num_accesses']
-        data[d][m] = data[d][m] + n if m in data[d] else n
-    with open('./../data/data_accesses_by_month.json', mode='w') as file:
-        json.dump(data, file, indent=2)
-
-
-def get_datasets_info(df, datasets_names, name_to_index):
-    print(f'Getting datasets info...', flush=True)
-    datasets_info = {}
-    for name in tqdm(datasets_names):
-        row = df.loc[name_to_index[name]]
-        datasets_info[name] = {'full_name': row['d_dataset'],
-                               'tot_size': row['dsize'],
-                               'num_files': row['nfiles'],
-                               'num_events': row['devts'],
-                               'data_tier': row['tier']}
-    return datasets_info
-
-
-def get_replicas_info(df, datasets_names):
-    print(f'Getting replicas info...', flush=True)
-    df = df[df['dataset_name'].isin(datasets_names)]
-    replicas_info = {dataset: {'num_replicas': len(dframe),
-                               'mean_size_replicas': dframe['rep_size'].mean(),
-                               'num_sites_replicas': dframe['node_name'].nunique()}
-                     for dataset, dframe in tqdm(df.groupby('dataset_name'))}
-    return replicas_info
-
-
-def get_accesses_info(datasets_names):
-    print(f'Getting accesses info...', flush=True)
-    with open('./../data/data_accesses_by_month.json', mode='r') as file:
-        data = json.load(file)
-    accesses_info = {dataset: {'mean_accesses/month': np.mean(list(data[dataset].values())),
-                               'std_accesses/month': np.std(list(data[dataset].values())),
-                               'median_accesses/month': np.median(list(data[dataset].values()))}
-                     for dataset in tqdm(datasets_names)}
-    return accesses_info
-
-
-def create_pop_features_parquet():
-    df1 = pd.read_parquet('./../data/datasets_list_sim.parquet')
-    dset_to_idx = {dset: idx
-                   for idx, dset in df1['d_dataset'].to_dict().items()}
-    df2 = pd.read_parquet('./../data/data_replicas_sim.parquet')
-    df3 = pd.read_parquet('./../data/data_accesses_sim.parquet')
-    with open('./../data/datasets_mcm_info.json', mode='r') as file:
-        mcm_raw = json.load(file)
+# Monte Carlo Management
+def create_mcm_features_json(datatier_to_mcm_file, datatier_to_outdir):
     pags = set(utils.get_pags())
-    mcm = {}
-    for dataset_name in mcm_raw:
-        dataset_info = mcm_raw[dataset_name]
-        pwg = dataset_info['pwg']
-        campaign = dataset_info['campaign']
-        generator = dataset_info['generator']
-        if pwg in pags and campaign != 'None' and generator != 'None':
-            mcm[dataset_name] = {'pag': pwg,
-                                 'campaign': campaign,
-                                 'generator': generator}
-    datasets_names = set.intersection(set(df1['d_dataset']), set(df2['dataset_name']),
-                                      set(df3['d_dataset']), set(mcm.keys()))
-    info1 = get_datasets_info(df1, datasets_names, dset_to_idx)
-    info2 = get_replicas_info(df2, datasets_names)
-    info3 = get_accesses_info(datasets_names)
-    print('Creating "pop_features.parquet"...', flush=True, end='')
-    keys1 = ['full_name', 'tot_size', 'num_files', 'num_events', 'data_tier']
-    mcm_keys = ['pag', 'campaign', 'generator']
-    keys2 = ['num_replicas', 'mean_size_replicas', 'num_sites_replicas']
-    keys3 = ['mean_accesses/month', 'std_accesses/month',
-             'median_accesses/month']
-    pop_dict = {dset_to_idx[dset]: [info1[dset][k] for k in keys1] +
-                [mcm[dset][k] for k in mcm_keys] +
-                [info2[dset][k] for k in keys2] +
-                [info3[dset][k] for k in keys3]
-                for dset in datasets_names}
-    cols = keys1 + mcm_keys + keys2 + keys3
-    pop_df = pd.DataFrame.from_dict(
-        pop_dict, orient='index', columns=cols).reset_index(drop=True)
-    pop_df.to_parquet('./../data/pop_features.parquet')
-    print(' Done')
+    for datatier in datatier_to_mcm_file:
+        message = f'\nCreating "{datatier}/mcm_features.json" file...'
+        print(message, flush=True)
+        infile = datatier_to_mcm_file[datatier]
+        mcm_data = {}
+        data = {}
+        mcm_data = utils.get_mcm_data(filepath=infile)
+        for dataset in tqdm(mcm_data):
+            pwg = utils.get_pwg_from_dataset(dataset, mcm_data)
+            campaign = utils.get_campaign_from_dataset(dataset, mcm_data)
+            generator = utils.get_generator_from_dataset(dataset, mcm_data)
+            if pwg != 'None' and campaign != 'None' and generator != 'None':
+                data[dataset] = {}
+                data[dataset]['pag'] = pwg if pwg in pags else 'Other PWG'
+                data[dataset]['campaign'] = campaign
+                data[dataset]['generator'] = generator
+        outfile = datatier_to_outdir[datatier] + 'mcm_features.json'
+        with open(outfile, mode='w') as f:
+            json.dump(data, f, indent=2)
+
+
+def get_datasets_names(datatier):
+    mcm_features_file = f'./../data/popularity/{datatier}/mcm_features.json'
+    dbs_features_file = f'./../data/popularity/{datatier}/dbs_features.json'
+    with open(mcm_features_file, mode='r') as f:
+        mcm_datasets_names = set(
+            d for d in json.load(f) if d.endswith(datatier.upper()))
+    with open(dbs_features_file, mode='r') as f:
+        dbs_datasets_names = set(
+            d for d in json.load(f) if d.endswith(datatier.upper()))
+    df1 = pd.read_parquet('./../data/parquet/dataset_site_info.parquet')
+    df1 = df1.drop(
+        columns=['dataset_id', 'replica_time_create', 'br_user_group_id']).dropna()
+    rep_datasets_names = set(
+        df1[df1['tier'] == datatier.upper()]['dataset_name'])
+    df2 = pd.read_parquet('./../data/parquet/dataset_reads.parquet').dropna()
+    df2['tier'] = df2['d_dataset'].apply(lambda name: name.split('/')[-1])
+    acc_datasets_names = set(
+        df2[df2['tier'] == datatier.upper()]['d_dataset'])
+    datasets_names = set.intersection(mcm_datasets_names, dbs_datasets_names,
+                                      rep_datasets_names, acc_datasets_names)
+    return datasets_names
+
+
+# Datasets replicas on T1/T2 disk
+def create_replicas_info_json(replicas_file, datatier_to_outdir):
+    replicas_df = pd.read_parquet(replicas_file)
+    replicas_df = replicas_df.drop(
+        columns=['dataset_id', 'replica_time_create', 'br_user_group_id', 'tier']).dropna()
+    for datatier in datatier_to_outdir:
+        message = f'\nCreating "{datatier}/replicas_info.json" file...'
+        print(message, flush=True)
+        datasets_names = get_datasets_names(datatier=datatier.upper())
+        dframe = replicas_df[replicas_df['dataset_name'].isin(datasets_names)]
+        for col in ['min_time', 'max_time']:
+            dframe[col] = dframe[col].apply(utils.get_int_from_date)
+        data = {}
+        for d, df in tqdm(dframe.groupby('dataset_name')):
+            data[d] = {'num_replicas': {},
+                       'size_replicas': {},
+                       'num_sites_replicas': {}}
+            start = utils.get_date_from_int(df['min_time'].min())
+            end = utils.get_date_from_int(df['max_time'].max())
+            date_range = [utils.get_int_from_date(dt.date())
+                          for dt in pd.date_range(start=start, end=end, freq='D')]
+            for date in date_range:
+                df_date = df[(df['min_time'] <= date) &
+                             (date <= df['max_time'])]
+                num_replicas = len(df_date)
+                if num_replicas > 0:
+                    size_replicas = round(df_date['rep_size'].sum())
+                    num_sites_replicas = df_date['node_name'].nunique()
+                    data[d][f'num_replicas'][date] = num_replicas
+                    data[d][f'size_replicas'][date] = size_replicas
+                    data[d][f'num_sites_replicas'][date] = num_sites_replicas
+        outfile = datatier_to_outdir[datatier] + 'replicas_info.json'
+        with open(outfile, mode='w') as f:
+            json.dump(data, f)
+
+
+# Datasets reads/accesses
+def create_accesses_info_json(accesses_file, datatier_to_outdir):
+    accesses_df = pd.read_parquet(accesses_file).dropna()
+    accesses_df['tier'] = accesses_df['d_dataset'].apply(
+        lambda name: name.split('/')[-1])
+    for datatier in datatier_to_outdir:
+        message = f'\nCreating "{datatier}/accesses_info.json" file...'
+        print(message, flush=True)
+        datasets_names = get_datasets_names(datatier=datatier.upper())
+        dframe = accesses_df[accesses_df['d_dataset'].isin(datasets_names)]
+        data = {d: {'num_accesses': {int(row['day'][2:]): utils.get_rounded_num_accesses(row['fract_read'])
+                                     for _, row in df.iterrows()}}
+                for d, df in tqdm(dframe.groupby('d_dataset'))}
+        outfile = datatier_to_outdir[datatier] + 'accesses_info.json'
+        with open(outfile, mode='w') as f:
+            json.dump(data, f, indent=2)
+
+
+def create_pop_dataframe_parquet(datatier_to_outdir):
+    for datatier in datatier_to_outdir:
+        message = f'\nCreating "{datatier}/pop_dataframe.parquet" file...'
+        print(message, end='')
+        key_to_file = {'dbs': f'./../data/popularity/{datatier}/dbs_features.json',
+                       'mcm': f'./../data/popularity/{datatier}/mcm_features.json',
+                       'rep':  f'./../data/popularity/{datatier}/replicas_info.json',
+                       'acc': f'./../data/popularity/{datatier}/accesses_info.json'}
+        key_to_data = {}
+        for key in key_to_file:
+            with open(key_to_file[key], mode='r') as file:
+                key_to_data[key] = json.load(file)
+        datasets_names = list(key_to_data['rep'].keys())
+        dataset_to_index = {dataset: index
+                            for index, dataset in enumerate(datasets_names)}
+        dict = {dataset_to_index[dataset]: {'dataset_name': dataset}
+                for dataset in datasets_names}
+        for dataset in datasets_names:
+            index = dataset_to_index[dataset]
+            for key in key_to_file:
+                for info in key_to_data[key][dataset]:
+                    dict[index][info] = key_to_data[key][dataset][info]
+        df = pd.DataFrame.from_dict(dict, orient='index')
+        outfile = datatier_to_outdir[datatier] + 'pop_dataframe.parquet'
+        df.to_parquet(path=outfile)
+        fix_pop_dataframe_parquet(path=outfile)
+        print(' Done')
+
+
+def fix_pop_dataframe_parquet(path):
+    df = pd.read_parquet(path)
+    for col in ['num_replicas', 'size_replicas', 'num_sites_replicas', 'num_accesses']:
+        df[col] = df[col].apply(lambda dict: {k: int(dict[k]) if dict[k] is not None else 0
+                                              for k in dict})
+    df['size_replicas'] = df.apply(lambda row: {day: row['size_replicas'][day] / row['tot_size']
+                                                for day in row['size_replicas']}, axis=1)
+    df.rename(columns={'size_replicas': 'fract_replicas'}, inplace=True)
+    df.to_parquet(path)
 
 
 if __name__ == '__main__':
 
-    print('\nSTEP-1')
-    create_datasets_list_sim_parquet()
-    create_data_replicas_sim_parquet()
-    create_data_accesses_sim_parquet()
+    datatiers = ['aodsim', 'miniaodsim', 'nanoaodsim']
+    dbs_file = './../data/parquet/dataset_size_info.parquet'
+    datatier_to_mcm_file = {
+        dt: f'./../data/mcm/{dt}_mcm_data.json' for dt in datatiers}
+    replicas_file = './../data/parquet/dataset_site_info.parquet'
+    accesses_file = './../data/parquet/dataset_reads.parquet'
+    datatier_to_outdir = {
+        dt: f'./../data/popularity/{dt}/' for dt in datatiers}
 
-    print('\nSTEP-2')
-    create_datasets_mcm_info_json()
-
-    print('\nSTEP-3')
-    create_data_replicas_on_disk_json()
-    remove_bad_accesses_from_parquet()
-    create_data_accesses_by_month_json()
-
-    print('\nSTEP-4')
-    create_pop_features_parquet()
+    create_pop_directories(datatier_to_outdir)
+    create_dbs_features_json(dbs_file, datatier_to_outdir)
+    create_mcm_features_json(datatier_to_mcm_file, datatier_to_outdir)
+    create_replicas_info_json(replicas_file, datatier_to_outdir)
+    create_accesses_info_json(accesses_file, datatier_to_outdir)
+    create_pop_dataframe_parquet(datatier_to_outdir)
